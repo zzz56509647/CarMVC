e19d1837919570fc197114c76794c4f07afd96d5
吕静 <@LLL-JJ>
2020-05-05 09:24:46 +0800

    11111

--- CarMVC/CarMVC/Usercss+js/images/login.js
+++ CarMVC/CarMVC/Usercss+js/images/login.js
@@ -1,66 +1,65 @@
-$(function(){
-	
-	$('#switch_qlogin').click(function(){
+$(function () {
+
+	$('#switch_qlogin').click(function () {
 		$('#switch_login').removeClass("switch_btn_focus").addClass('switch_btn');
 		$('#switch_qlogin').removeClass("switch_btn").addClass('switch_btn_focus');
-		$('#switch_bottom').animate({left:'0px',width:'70px'});
-		$('#qlogin').css('display','none');
-		$('#web_qr_login').css('display','block');
-		
-		});
-	$('#switch_login').click(function(){
-		
-		$('#switch_login').removeClass("switch_btn").addClass('switch_btn_focus');
-		$('#switch_qlogin').removeClass("switch_btn_focus").addClass('switch_btn');
-		$('#switch_bottom').animate({left:'154px',width:'70px'});
-		
-		$('#qlogin').css('display','block');
-		$('#web_qr_login').css('display','none');
-		});
-if(getParam("a")=='0')
-{
-	$('#switch_login').trigger('click');
-}
+		$('#switch_bottom').animate({ left: '0px', width: '70px' });
+		$('#qlogin').css('display', 'none');
+		$('#web_qr_login').css('display', 'block');
 
 	});
-	
-function logintab(){
+	$('#switch_login').click(function () {
+
+		$('#switch_login').removeClass("switch_btn").addClass('switch_btn_focus');
+		$('#switch_qlogin').removeClass("switch_btn_focus").addClass('switch_btn');
+		$('#switch_bottom').animate({ left: '154px', width: '70px' });
+
+		$('#qlogin').css('display', 'block');
+		$('#web_qr_login').css('display', 'none');
+	});
+	if (getParam("a") == '0') {
+		$('#switch_login').trigger('click');
+	}
+
+});
+
+function logintab() {
 	scrollTo(0);
 	$('#switch_qlogin').removeClass("switch_btn_focus").addClass('switch_btn');
 	$('#switch_login').removeClass("switch_btn").addClass('switch_btn_focus');
-	$('#switch_bottom').animate({left:'154px',width:'96px'});
-	$('#qlogin').css('display','none');
-	$('#web_qr_login').css('display','block');
-	
+	$('#switch_bottom').animate({ left: '154px', width: '96px' });
+	$('#qlogin').css('display', 'none');
+	$('#web_qr_login').css('display', 'block');
+
 }
 
 
 //根据参数名获得该参数 pname等于想要的参数名 
-function getParam(pname) { 
-    var params = location.search.substr(1); // 获取参数 平且去掉？ 
-    var ArrParam = params.split('&'); 
-    if (ArrParam.length == 1) { 
-        //只有一个参数的情况 
-        return params.split('=')[1]; 
-    } 
-    else { 
-         //多个参数参数的情况 
-        for (var i = 0; i < ArrParam.length; i++) { 
-            if (ArrParam[i].split('=')[0] == pname) { 
-                return ArrParam[i].split('=')[1]; 
-            } 
-        } 
-    } 
-}  
+function getParam(pname) {
+	var params = location.search.substr(1); // 获取参数 平且去掉？ 
+	var ArrParam = params.split('&');
+	if (ArrParam.length == 1) {
+		//只有一个参数的情况 
+		return params.split('=')[1];
+	}
+	else {
+		//多个参数参数的情况 
+		for (var i = 0; i < ArrParam.length; i++) {
+			if (ArrParam[i].split('=')[0] == pname) {
+				return ArrParam[i].split('=')[1];
+			}
+		}
+	}
+}
 
 
 var reMethod = "GET",
 	pwdmin = 6;
 
-$(document).ready(function() {
+$(document).ready(function () {
 
 
-	$('#reg').click(function() {
+	$('#reg').click(function () {
 
 		if ($('#user').val() == "") {
 			$('#user').focus().css({
@@ -88,13 +87,13 @@ $(document).ready(function() {
 			url: "/member/ajaxyz.php",
 			data: "uid=" + $("#user").val() + '&temp=' + new Date(),
 			dataType: 'html',
-			success: function(result) {
+			success: function (result) {
 
 				if (result.length > 2) {
 					$('#user').focus().css({
 						border: "1px solid red",
 						boxShadow: "0 0 2px red"
-					});$("#userCue").html(result);
+					}); $("#userCue").html(result);
 					return false;
 				} else {
 					$('#user').css({
@@ -107,34 +106,27 @@ $(document).ready(function() {
 		});
 
 
-		if ($('#passwd').val().length < pwdmin) {
-			$('#passwd').focus();
-			$('#userCue').html("<font color='red'><b>×密码不能小于" + pwdmin + "位</b></font>");
-			return false;
-		}
-		if ($('#passwd2').val() != $('#passwd').val()) {
-			$('#passwd2').focus();
-			$('#userCue').html("<font color='red'><b>×两次密码不一致！</b></font>");
-			return false;
-		}
 
-		var sqq = /^[1-9]{1}[0-9]{4,9}$/;
-		if (!sqq.test($('#qq').val()) || $('#qq').val().length < 5 || $('#qq').val().length > 12) {
-			$('#qq').focus().css({
+
+		var mail = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+		if (!mail.test($('#mail').val())) {
+			$('#mail').focus().css({
 				border: "1px solid red",
-				boxShadow: "0 0 2px red"
+				boxShadow: "0 0 2px red",
+				
 			});
-			$('#userCue').html("<font color='red'><b>×QQ号码格式不正确</b></font>");return false;
-		} else {
+			$('#userCue').html("<font color='red'><b>×邮箱格式错误！</b></font>");
+		}
+		else {
 			$('#qq').css({
 				border: "1px solid #D7D7D7",
 				boxShadow: "none"
 			});
-			
+
 		}
 
 		$('#regUser').submit();
 	});
-	
+
 
 });
